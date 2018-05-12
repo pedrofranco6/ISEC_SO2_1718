20f37f2fe91d9c0c0794b5f16d3ab88381109f0a
unknown <pedrofranco6@hotmail.com>
2018-05-12 19:00:52 +0100

    first commit

---
+++ TrabalhoSO2/Servidor/Servidor.cpp
@@ -0,0 +1,16 @@
+// Servidor.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+
+int main()
+{
+	char string[100];
+
+	printf("asd");
+	scanf_s("%s", &string);
+
+    return 0;
+}
+
